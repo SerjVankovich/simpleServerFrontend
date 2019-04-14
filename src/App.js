0699ecfae70c8bc8bd408937f24ffdf6dd24d5b5
Sergey Vankovich <SerjVankovich@github.com>
2019-04-14 10:19:55 +1000

    Initially commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header"
+import Post from "./components/Post/Post"
+import Loading from "./components/Loading/Loading"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      posts: []
+    }
+
+    this.getPosts = this.getPosts.bind(this)
   }
+
+  componentDidMount() {
+    this.getPosts()
+  }
+
+  getPosts() {
+    fetch("http://localhost:8080")
+      .then(
+          res => res.ok ? res.json() : Promise.reject(res)
+        )
+        .then(
+          body => this.setState({ posts: body })
+        )
+          .catch(err => console.error(err))
+  }
+
+  render() {
+    const { posts } = this.state
+    return (
+      <div>
+        <Header text="Welcome to the simple mirror of the reddit"/>
+        { posts.length === 0 
+        ?
+          <Loading/>
+          :
+
+          posts.map((post, id) => (
+            <Post post={ post } key={id}/>
+          ))
+         }
+        
+      </div>
+      
+    )
+  }
+
 }
+  
 
 export default App;
---
+++ src/components/Post/Post.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./Post.css"
+import unknown from "./icon-of-unknown-person.png"
+
+const Post = ({ post }) => (
+    <div className="bg">
+        <hr/>
+        <h6>
+            {
+                post.authorImgSrc
+                 ? 
+                 <img className="authorImg" src={ post.authorImgSrc } alt="author-img"/>
+
+                 :
+                <img className="authorImg" src={unknown} alt="unknown person"/>
+            }
+            
+            <a className="authorLink" href={ post.authorLink }>{post.author}</a>
+        </h6>
+        <h3>
+            <a className="title" href={ post.link }>{ post.title }</a>
+        </h3>
+        { post.imgSrc &&
+                <img className="img" src={ post.imgSrc } alt="post image"/>
+            }
+        <hr/>
+    </div>
+    
+)
+
+export default Post
